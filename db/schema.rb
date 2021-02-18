34dbe255b337fc889775763a4764153febff79e4
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-02-18 11:02:53 -0500

    migrate db to include info model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_17_140837) do
+ActiveRecord::Schema.define(version: 2021_02_17_144445) do
+
+  create_table "infos", force: :cascade do |t|
+    t.integer "venue_id"
+    t.integer "capacity"
+    t.string "dates"
+    t.float "rentalfee"
+    t.float "ceremonyfee"
+    t.float "perperson"
+    t.float "otherpp"
+    t.text "notes"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
