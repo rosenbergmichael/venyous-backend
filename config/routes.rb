cfc32b6c8973ca58749b740f28cf22f3c95fbce9
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-02-19 11:30:22 -0500

    add infos resource to api v1 namespace in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
 
   
 
-  resources :infos
+  
   namespace :api do
     namespace :v1 do 
+      resources :infos
       resources :venues
       resources :users
     end
